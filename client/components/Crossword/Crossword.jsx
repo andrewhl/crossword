6c51674bc95f42fcaf5816520c57ebb8bf4043d0
Arturo Guzman <arturo@kisurfer.com>
2016-02-15 17:08:37 -0500

    add line and column numbers to cross word component

--- client/components/Crossword/Crossword.jsx
+++ client/components/Crossword/Crossword.jsx
@@ -1,39 +1,43 @@
-import React, { Component } from 'react';
+import React, { PropTypes } from 'react';
 import R from 'ramda';
 
 const mapWithIndex = R.addIndex(R.map);
 
-export default class Crossword extends Component {
+export default class Crossword extends React.Component {
+  static propTypes = {
+    data: PropTypes.array,
+  };
 
-  render() {
-    const rows = mapWithIndex(this.createRow.bind(this), this.props.data);
+  renderColumnNumbersRow = () =>
+    <tr>
+      <td></td>
+      {mapWithIndex(i => <td key={i} className="number">{i + 1}</td>, R.range(0, 15))}
+    </tr>;
 
-    return (
-      <table>
-        <tbody>
-          {rows}
-        </tbody>
-      </table>
-    )
-  }
+  renderRow = (row, index) =>
+    <tr key={index}>
+      <td className="number">{index + 1}</td>
+      {mapWithIndex(this.renderCell.bind(this), row)}
+    </tr>;
 
-  createRow(row, index) {
-    const cells = mapWithIndex(this.createCell.bind(this), row);
-
-    return (
-      <tr key={index}>
-        {cells}
-      </tr>
-    )
-  }
-
-  createCell(cell, index) {
+  renderCell = (cell, index) => {
     const className = (cell !== '') ? '' : 'empty';
 
     return (
       <td key={index}>
         <div className={className}>{cell}</div>
       </td>
-    )
+    );
+  };
+
+  render() {
+    return (
+      <table>
+        <tbody>
+          {this.renderColumnNumbersRow()}
+          {mapWithIndex(this.renderRow.bind(this), this.props.data)}
+        </tbody>
+      </table>
+    );
   }
 }
