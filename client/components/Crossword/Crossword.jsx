334db601c957a2f41db552e092e400d53054e783
Arturo Guzman <arturo@kisurfer.com>
2016-02-16 23:35:44 -0500

    fix linting issues

--- client/components/Crossword/Crossword.jsx
+++ client/components/Crossword/Crossword.jsx
@@ -3,6 +3,32 @@ import R from 'ramda';
 
 const mapWithIndex = R.addIndex(R.map);
 
+const numberify = string => {
+  // R.replace(/(1)+/g, )
+  console.log('Numberify:', string);
+
+  // const oneCounts = R.split('0', string).map(ones => {
+  //   return ones.length;
+  // });
+  let computedString = [];
+  let tempCount = 0;
+  R.map(letter => {
+    console.log(tempCount);
+    if (parseInt(letter, 10) === 0) {
+      computedString.push(tempCount);
+      tempCount = 0;
+    }
+    tempCount += parseInt(letter, 10);
+  }, string);
+  // R.addIndex(R.forEach)((item, index) => {
+  // }, string);
+
+  return computedString;
+
+  // return oneCounts;
+  // return string.match(/(1)+/g)[2].length;
+};
+
 export default class Crossword extends React.Component {
   static propTypes = {
     data: PropTypes.array,
@@ -12,16 +38,20 @@ export default class Crossword extends React.Component {
     <tr>
       <td></td>
       {mapWithIndex(i => <td key={i} className="number">{i + 1}</td>, R.range(0, 15))}
-    </tr>;
+    </tr>
+  ;
 
   renderRow = (row, index) =>
     <tr key={index}>
       <td className="number">{index + 1}</td>
       {mapWithIndex(this.renderCell.bind(this), row)}
-    </tr>;
+    </tr>
+  ;
 
   renderCell = (cell, index) => {
-    const className = (cell !== '') ? '' : 'empty';
+    const className = (cell !== '')
+      ? ''
+      : 'empty';
 
     return (
       <td key={index}>
@@ -31,6 +61,9 @@ export default class Crossword extends React.Component {
   };
 
   render() {
+    console.log('Numberify: ', numberify('01101110111111'));
+
+    // console.log('Numberify: ', numberify('11101110111111'));
     return (
       <table>
         <tbody>
