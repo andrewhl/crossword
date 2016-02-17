5bb7d0cb0b451e6ea932e53b9fcfb744380c0360
Arturo Guzman <arturo@kisurfer.com>
2016-02-17 00:22:05 -0500

    move unused code to archive

--- client/components/Index/Index.jsx
+++ client/components/Index/Index.jsx
@@ -1,79 +1,14 @@
 import React, { Component } from 'react';
+
 import Crossword from '../Crossword/Crossword.jsx';
 import { generateCrosswordPattern } from '../../../lib/crossword_utils';
-import R from 'ramda';
-
-const crosswordData = [
-  {
-    position: [0, 0],
-    word: 'anapest',
-    direction: 'across',
-  },
-  {
-    position: [8, 0],
-    word: 'drachma',
-    direction: 'across',
-  },
-  {
-    position: [0, 0],
-    word: 'arch',
-    direction: 'down',
-  }
-];
-
-const buildLetterObject = (startX, startY, dir, letter, idx) => {
-  return {
-    letter,
-    x: dir === 'across' ? startX + idx : startX,
-    y: dir === 'down' ? startY + idx : startY,
-  };
-};
-
-const initialize2dArray = (size) => {
-  let crossword = [];
-  for (var i = 0; i < size; i++) {
-    crossword.push([]);
-    for (var j = 0; j < size; j++) {
-      crossword[i].push('');
-    }
-  }
-  return crossword;
-}
-
-const getLetterObjsFromWordList = wordList => R.flatten(R.map(getLetterObjs, wordList));
-
-const getDataFromLetterObjs = letterObjs => {
-  let crossword = initialize2dArray(15);
-  R.forEach((letterObj) => {
-    crossword[letterObj.y][letterObj.x] = letterObj.letter;
-  }, letterObjs);
-  return crossword;
-};
-
-const process = R.compose(getDataFromLetterObjs, getLetterObjsFromWordList);
-
-const getLetterObjs = ({ word, position, direction }) => {
-    const startX = position[0];
-    const startY = position[1];
-    const curriedBuildLetterObject = R.curry(buildLetterObject)(startX, startY, direction);
-
-    const mapIndexed = R.addIndex(R.map);
-    return mapIndexed(curriedBuildLetterObject, word.split(''));
-};
-
-const splitCrossWordPattern = pattern => pattern.map(p => p.split('').map(t => parseInt(t, 10)));
-const processedCrossword = splitCrossWordPattern(generateCrosswordPattern()).map(row => row.map(c => c === 1 ? c : ''));
 
 class IndexComponent extends Component {
   render() {
     return (
-      <Crossword data={processedCrossword} />
+      <Crossword crossword={generateCrosswordPattern()} />
     );
   }
 }
-
-IndexComponent.defaultProps = {
-  items: []
-};
 
 export default IndexComponent;
